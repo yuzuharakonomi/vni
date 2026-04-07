3671ab1886f6b1ae83f87841eeb79c3eb6c742bc
Konomi Yuzuhara <konomi@yuzuhara.net>
2026-04-07 22:40:45 +0900

    Make content.config.ts

---
+++ src/content.config.ts
@@ -0,0 +1,8 @@
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/data/blog" }),
+});
+
+export const collections = { blog };
