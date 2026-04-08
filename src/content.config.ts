d56a2528c820663bcedef635bc86eb13d4c25ea0
Konomi Yuzuhara <konomi@yuzuhara.net>
2026-04-08 22:18:04 +0900

    Define blog schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,14 @@
 import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/data/blog" }),
+  schema: z.object({
+    title: z.string().min(1),
+    createdAt: z.iso.date(),
+    updatedAt: z.iso.date(),
+  }),
 });
 
 export const collections = { blog };
